2ff5080fef844eb753729755956d6aa7c28fef8e
vk1719676-byte <vk1719676@gmail.com>
2025-08-30 12:15:24 +0530

    Delete src/components/notifications directory

--- src/components/notifications/index.ts
+++
@@ -1,4 +0,0 @@
-export { NotificationSystem } from './NotificationSystem';
-export { NotificationBell } from './NotificationBell';
-export { NotificationDropdown } from './NotificationDropdown';
-export { NotificationItem } from './NotificationItem';
